c532c4c708a55971fcd8dd0dc20bd3a77a6a022d
Brian Lambert <brianlambert2408@gmail.com>
2022-12-16 10:18:08 +1100

    commit tree 9d49ae04d17610799ed18b1bd2cfd5d766c98460

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+    def show
+        user = User.find_by id: session[:user_id]
+
+        if user
+            render json: user, status: :ok
+        else
+            render json: {error: "Unknown user "}, status: :unauthorized
+        end
+
+    end
+
+end
